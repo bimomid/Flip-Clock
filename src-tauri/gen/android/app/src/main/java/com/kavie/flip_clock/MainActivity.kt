1166a349683f0a954ed0226aa823b13560039e79
LIDFSL <2849489704@qq.com>
2026-04-22 17:41:35 +0800

    初创项目

---
+++ src-tauri/gen/android/app/src/main/java/com/kavie/flip_clock/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kavie.flip_clock
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
